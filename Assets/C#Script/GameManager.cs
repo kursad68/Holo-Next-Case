a87dcb05058c3b9b9673234472e65da533715b5c
kursad68 <aykinlar6806@gmail.com>
2021-11-19 10:06:14 +0300

    71

---
+++ Assets/C#Script/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject Rocks;
+    public List<GameObject> objectFriend;
+    public List<GameObject> objectEnemy;
+    private void OnEnable()
+    {
+        EventManager.getGameManager += gm;
+    }
+    private void OnDisable()
+    {
+
+        EventManager.getGameManager -= gm;
+    }
+
+    GameManager gm()
+    {
+        return GetComponent<GameManager>();
+    }
+
+  
+}
